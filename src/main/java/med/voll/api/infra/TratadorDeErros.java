cf3107d7adae9d1fa19629f2e0d89ee647ce75d2
nelsonFilho22222 <nelsoncamarafilho@gmail.com>
2024-06-21 16:36:27 -0300

    API_FULL_500_404_400

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -2,8 +2,10 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.*;
 
+import org.springframework.web.bind.annotation.*;
 
 
 @RestControllerAdvice
@@ -11,7 +13,31 @@ public class TratadorDeErros {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
+
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+
+        var erros = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+
+
+
+
+
+
+    private record DadosErroValidacao(String campo,
+                                      String mensagem) {
+
+        public DadosErroValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+
     }
 
 }
